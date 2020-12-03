1e316d72e65fdb6c6109054198e295aef44e33b6
Wolfgang Senfter <w.senfter@gmail.com>
2020-12-03 22:25:47 +0100

    defne variable

--- conf.d/deno.fish
+++ conf.d/deno.fish
@@ -1,19 +1,20 @@
 set -x DENO_INSTALL "$HOME/.deno"
 set -x PATH $PATH "$DENO_INSTALL/bin"
 
+set DENO_COMPLETIONS_FILE "$HOME/.config/fish/completions/deno.fish"
+
 function _deno_install
     curl -fsSL "https://deno.land/x/install/install.sh" | sh
 end
 
 function _deno_uninstall
-    set completions_file = "$HOME/.config/fish/completions/deno.fish"
-    if test -f $completions_file
-        rm $completions_file
+    if test -f "$DENO_COMPLETIONS_FILE"
+        rm "$DENO_COMPLETIONS_FILE"
     end
 end
 
 function _deno_completions
-    deno completions fish >"$HOME/.config/fish/completions/deno.fish"
+    deno completions fish >"$DENO_COMPLETIONS_FILE"
 end
 
 function _on_deno_install --on-event deno_install
