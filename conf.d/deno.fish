cff6e32b737488b8c383dc409d9f8d2ab5cb7a94
Wolfgang Senfter <w.senfter@gmail.com>
2020-12-03 22:12:29 +0100

    Moved code to functions

--- conf.d/deno.fish
+++ conf.d/deno.fish
@@ -1,17 +1,25 @@
 set -x DENO_INSTALL "$HOME/.deno"
 set -x PATH $PATH "$DENO_INSTALL/bin"
 
-function _deno_update_completions
-    deno completions fish >"$HOME/.config/fish/completions/deno.fish"
+function _deno_install
+    curl -fsSL https://deno.land/x/install/install.sh | sh
 end
 
-if not test -f "$DENO_INSTALL/bin/deno"
-    curl -fsSL https://deno.land/x/install/install.sh | sh
-    _deno_update_completions
+function _deno_update_completions
+    deno completions fish >"$HOME/.config/fish/completions/deno.fish"
 end
 
 function _post_deno_upgrade --on-event fish_postexec
     if string match -qr '^deno upgrade$' $argv
         _deno_update_completions
     end
+end
+
+function _deno_uninstall --on-event deno_uninstall
+    rm "$HOME/.config/fish/completions/deno.fish"
+end
+
+if not test -f "$DENO_INSTALL/bin/deno"
+    _deno_install
+    _deno_update_completions
 end
