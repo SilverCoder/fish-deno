eb5ff1895f430abd5e54e254450e9b03d150a42c
Wolfgang Senfter <w.senfter@gmail.com>
2020-12-03 22:14:37 +0100

    Check if file exists

--- conf.d/deno.fish
+++ conf.d/deno.fish
@@ -5,6 +5,12 @@ function _deno_install
     curl -fsSL https://deno.land/x/install/install.sh | sh
 end
 
+function _deno_uninstall --on-event deno_uninstall
+    if test "$HOME/.config/fish/completions/deno.fish"
+        rm "$HOME/.config/fish/completions/deno.fish"
+    end
+end
+
 function _deno_update_completions
     deno completions fish >"$HOME/.config/fish/completions/deno.fish"
 end
@@ -13,10 +19,6 @@ function _post_deno_upgrade --on-event fish_postexec
     if string match -qr '^deno upgrade$' $argv
         _deno_update_completions
     end
-end
-
-function _deno_uninstall --on-event deno_uninstall
-    rm "$HOME/.config/fish/completions/deno.fish"
 end
 
 if not test -f "$DENO_INSTALL/bin/deno"
