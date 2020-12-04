9ba42453ac8f301871355545b4711c3016b2e2f6
Wolfgang Senfter <w.senfter@gmail.com>
2020-12-04 20:52:47 +0100

    Added placeholder file

--- conf.d/deno.fish
+++ conf.d/deno.fish
@@ -7,12 +7,6 @@ function _deno_install
     curl -fsSL "https://deno.land/x/install/install.sh" | sh
 end
 
-function _deno_uninstall
-    if test -f "$DENO_COMPLETIONS_FILE"
-        rm "$DENO_COMPLETIONS_FILE"
-    end
-end
-
 function _deno_completions
     deno completions fish >"$DENO_COMPLETIONS_FILE"
 end
@@ -23,10 +17,6 @@ function _on_deno_install --on-event deno_install
     end
 
     _deno_completions
-end
-
-function _on_deno_uninstall --on-event deno_uninstall
-    _deno_uninstall
 end
 
 function _on_postexec_deno_upgrade --on-event fish_postexec
